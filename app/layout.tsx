15e0f63a6029f4e5f4a7d1667f6c0e8bd353da68
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-10-27 23:33:23 -0400

    Landing page Pc View
    Refactored 768px, 992px, 1200px and added 1440px

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
 import './globals.css'
 import type { Metadata } from 'next'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Uptech Incorporated Limited',
+  applicationName:"Uptech Incorporated Limited",
+  authors: {
+    url: "https://www.linkedin.com/company/uptech-incorporated/",
+    name: "Uptech Incorporated Limited"
+  },
+  keywords: ["ourlime", "ourlime.com", "uptech", "uptech.com", "uptechincorp", "uptechincorp.com" ,"social media platform", "online community", "social networking", "connect with friends", "social media app", "share moments", "online interaction", "follow and share", "interactive platform", "social engagement", "online socializing", "social media connection", "create and share", "discover new content", "social feed", "personalized feed", "connect with people", "social media profiles", "social interaction online", "online community platform"],
+
   description: 'Corporate Website',
-  image: "/public/favicon.ico"
 }
 
 export default function RootLayout({
