75ddfffc0058e2d2cf4adbdcbc196452396c28b7
Raushawn <105221990+Raushawn@users.noreply.github.com>
2023-12-13 14:16:16 -0400

    navbar on mobile shows up now

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,25 @@
+'use client'
+
 import '../styles/navbar.css'
 
+import { useRef } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
+import {FaBars, FaTimes} from "react-icons/fa" 
+
 import upTechLogo from '../public/images/uptech logo.png'
 import menu from '../public/images/menu.svg'
+import React from 'react'
 export default function Navbar() {
+
+  const navRef = useRef<HTMLElement>(null);
+
+  const showNavbar = () =>{
+    if(navRef.current){
+    navRef.current.classList.toggle("responsive_nav");
+  }
+  };
 
   return (
     <header className="header">
@@ -18,17 +32,18 @@ export default function Navbar() {
             />
         </div>
 
-        <nav className="navbar">
+        <nav className="navbar" ref={navRef}>
           <ul className="nav-items">
               <li className="nav-links"><Link href="/">Home</Link></li>
               <li className="nav-links"><Link href="/objectives">Objectives</Link></li>
               <li className="nav-links"><Link href="/about">About us</Link></li>
               <li className="nav-links"><Link href="/contact">Contact us</Link></li>
+              <li><button className='nav-btn nav-close-btn' onClick={showNavbar}><FaTimes/></button></li>
           </ul>
         </nav>
         
-        <Image className='menu-btn' src={menu} alt="menu"/>
-    
+        {/* <Image className='menu-btn' src={menu} alt="menu"/> */}
+        <button className='nav-btn' onClick={showNavbar}><FaBars/></button>
   </header>
   )
 }
