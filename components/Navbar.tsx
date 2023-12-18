22bab2a568a77d0ca6e9070ff95e6d903c82be9a
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-12-18 12:49:49 -0400

    Fixed navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,19 +2,21 @@
 
 import '../styles/navbar.css'
 
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useWindowSize } from 'react-use'; //react package that checks the screen size of the device
 
-import {FaBars, FaTimes} from "react-icons/fa" 
+import {FaBars} from "react-icons/fa" 
 
 import upTechLogo from '../public/images/uptech logo.png'
 import dropdown from '../public/images/Vector 89.svg'
-import React from 'react'
+import menuBtn from '@/public/images/menu.svg'
+import { useWindowSize } from 'react-use'; //react package that checks the screen size of the device
+
 
 export default function Navbar() {
     const [isNavVisible, setIsNavVisible] = useState(false);
+    const { width } = useWindowSize();
 
     const toggleNav = () => {
         setIsNavVisible(!isNavVisible);
@@ -27,36 +29,32 @@ export default function Navbar() {
                     src={upTechLogo}
                     alt="logo"
                     priority={true}
-                    layout="responsive"
                 />
             </div>
 
             <button className='nav-btn' onClick={toggleNav}>
-                {isNavVisible ? <Image src={dropdown} alt="close nav" layout="responsive" /> : <FaBars />}
+                <Image src={isNavVisible ?  dropdown : menuBtn} alt="close nav" />
             </button>
 
-            <nav className={`navbar ${isNavVisible ? 'show' : ''}`}>
-                <ul className="nav-items">
-                    <li className="nav-links"><Link href="/">Home</Link></li>
-                    <li className="nav-links"><Link href="/objectives">Objectives</Link></li>
-                    <li className="nav-links"><Link href="/about">About us</Link></li>
-                    <li className="nav-links"><Link href="/contact">Contact us</Link></li>
-                </ul>
-            </nav>
-
-            <nav className={`dropdown ${isNavVisible ? 'show-dropdown' : ''}`}>
-                <ul className="dropdown-menu">
-                    <header className='top-line'>
-                        <h2 className="nav-head">NavBar</h2>
-                        <button className='nav-close-btn' onClick={toggleNav}>
-                        </button>
-                    </header>
-                    <li className="nav-links"><Link href="/">Home</Link></li>
-                    <li className="nav-links"><Link href="/objectives">Objectives</Link></li>
-                    <li className="nav-links"><Link href="/about">About us</Link></li>
-                    <li className="nav-links"><Link href="/contact">Contact us</Link></li>
-                </ul>
-            </nav>
+            {width >= 768 ? (
+                <nav className='navbar'>
+                    <ul className="nav-items">
+                        <li className="nav-links"><Link href="/">Home</Link></li>
+                        <li className="nav-links"><Link href="/objectives">Objectives</Link></li>
+                        <li className="nav-links"><Link href="/about">About us</Link></li>
+                        <li className="nav-links"><Link href="/contact">Contact us</Link></li>
+                    </ul>
+                </nav>
+            ) : (
+                <nav className={`dropdown ${isNavVisible ? 'show-dropdown' : ''}`}>
+                    <ul className="dropdown-menu">
+                        <li className="nav-links"><Link href="/">Home</Link></li>
+                        <li className="nav-links"><Link href="/objectives">Objectives</Link></li>
+                        <li className="nav-links"><Link href="/about">About us</Link></li>
+                        <li className="nav-links"><Link href="/contact">Contact us</Link></li>
+                    </ul>
+                </nav>
+            )}
         </header>
     );
 }
