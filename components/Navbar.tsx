e86f771c131feea178edfb8c9a5c1dbe040c5881
Raushawn <raushawnmitchell@gmail.com>
2024-01-25 22:57:04 -0400

    navbar animation added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,6 +7,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import {FaBars} from "react-icons/fa" 
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 import upTechLogo from '../public/images/uptech logo.png'
 import dropdown from '../public/images/Vector 89.svg'
@@ -18,12 +20,25 @@ export default function Navbar() {
     const [isNavVisible, setIsNavVisible] = useState(false);
     const { width } = useWindowSize();
 
+    const [navRef, navInView] = useInView({triggerOnce: true, threshold: 0.1});
+    
+    const verticalVariants = {
+        hidden: { y: '-100%', opacity: 0 }, // Starts above the screen and invisible
+        visible: { y: 0, opacity: 1 },      // Ends at its normal position and visible
+      };
+
     const toggleNav = () => {
         setIsNavVisible(!isNavVisible);
     };
 
     return (
         <header className="header">
+            <motion.div
+                ref={navRef}
+                initial="hidden"
+                animate={navInView ? "visible" : "hidden"}
+                variants={verticalVariants}
+                transition={{duration: 1}}>
             <div className="logo-wrapper">
                 <Image
                     src={upTechLogo}
@@ -31,6 +46,7 @@ export default function Navbar() {
                     priority={true}
                 />
             </div>
+            </motion.div>
 
             <button className='nav-btn' onClick={toggleNav}>
                 <Image src={isNavVisible ?  dropdown : menuBtn} alt="close nav" />
@@ -38,12 +54,19 @@ export default function Navbar() {
 
             {width >= 768 ? (
                 <nav className='navbar'>
+                    <motion.div
+                ref={navRef}
+                initial="hidden"
+                animate={navInView ? "visible" : "hidden"}
+                variants={verticalVariants}
+                transition={{duration: 1}}>
                     <ul className="nav-items">
                         <li className="nav-links"><Link href="/">Home</Link></li>
                         <li className="nav-links"><Link href="/objectives">Objectives</Link></li>
                         <li className="nav-links"><Link href="/about">About us</Link></li>
                         <li className="nav-links"><Link href="/contact">Contact us</Link></li>
                     </ul>
+                    </motion.div>
                 </nav>
             ) : (
                 <nav className={`dropdown ${isNavVisible ? 'show-dropdown' : ''}`}>
