ff88ff75ef778873f69b8a2dcf255e23e7631268
Saeed Siddeeq Sinanan <saeedsinanan23@gmail.com>
2025-01-06 19:40:03 -0400

    inprogress

--- components/JobApplicationModal.tsx
+++ components/JobApplicationModal.tsx
@@ -1,23 +1,62 @@
+import React, { useState } from 'react';
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../firebase/firebase';
+
 export default function JobApplicationModal(
-    { isOpen, onClose, positions }: 
-    { 
-        isOpen: boolean; 
-        onClose: () => void; 
-        positions: Array<string> 
-    }){
-    if (!isOpen) return null;
+  { isOpen, onClose, positions }: 
+  { 
+    isOpen: boolean; 
+    onClose: () => void; 
+    positions: Array<string> 
+  }) {
+  const [portfolioURL, setPortfolioURL] = useState('');
+  const [selectedPosition, setSelectedPosition] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await addDoc(collection(db, 'applications'), {
+      firstName,
+      lastName,
+      contactNumber,
+      email,
+      portfolioURL,
+      position: selectedPosition,
+    });
+
+    onClose();
+  };
+
+  if (!isOpen) return null;
   
     return (
       <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-10">
         <div className="bg-white p-8 rounded-lg w-96">
           <h2 className="text-2xl font-bold mb-4">Application Form</h2>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="mb-2">
               <input
                 type="text"
-                name="name"
-                placeholder="First and Last Name"
-                className="w-full bg-blueTheme text-white placeholder-gray-100 border rounded px-2 py-1 "
+                name="first name"
+                placeholder="First Name"
+                className="w-full bg-blueTheme text-white placeholder-gray-100 border rounded px-2 py-1"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-2">
+            <input
+                type="text"
+                name="last name"
+                placeholder="Last Name"
+                className="w-full bg-blueTheme text-white placeholder-gray-100 border rounded px-2 py-1"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
                 required
               />
             </div>
@@ -25,6 +64,8 @@ export default function JobApplicationModal(
               <select
                 name="position"
                 className="w-full bg-blueTheme text-white placeholder-gray-100 border rounded px-2 py-1"
+                value={selectedPosition}
+                onChange={(e) => setSelectedPosition(e.target.value)}
                 required
               >
                 <option value="" disabled selected>
@@ -43,6 +84,8 @@ export default function JobApplicationModal(
                 name="number"
                 placeholder="Contact Number"
                 className="w-full bg-blueTheme text-white placeholder-gray-100 border rounded px-2 py-1"
+                value={contactNumber}
+                onChange={(e) => setContactNumber(e.target.value)}
                 required
               />
             </div>
@@ -52,7 +95,37 @@ export default function JobApplicationModal(
                 name="email"
                 placeholder="Email"
                 className="w-full bg-blueTheme text-white placeholder-gray-100 border rounded px-2 py-1"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
+              />
+            </div>
+            <div className="mb-2">
+            <label className="block text-black mb-1" htmlFor="resume">Upload Resume</label>
+              <input
+                type="file"
+                name="resume"
+                placeholder="Upload Resume"
+                className="w-full bg-blueTheme text-white placeholder-gray-100 border rounded px-2 py-1"
+              />
+            </div>
+            <div className="mb-2">
+            <label className="block text-black mb-1" htmlFor="resume">Upload Portfolio</label>
+              <input
+                type="file"
+                name="portfolioFile"
+                placeholder="Upload Portfolio"
+                className="w-full bg-blueTheme text-white placeholder-gray-100 border rounded px-2 py-1"
+              />
+            </div>
+            <div className="mb-2">
+              <input
+                type="url"
+                name="portfolio"
+                placeholder="Portfolio URL"
+                className="w-full bg-blueTheme text-white placeholder-gray-100 border rounded px-2 py-1"
+                value={portfolioURL}
+                onChange={(e) => setPortfolioURL(e.target.value)}
               />
             </div>
             <div className="flex justify-between">
